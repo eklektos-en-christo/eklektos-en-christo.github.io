ba9c829d55eef427c4593ac5eb72901780598429
eklektos-en-christo <itzgroveranujmaseeh51648542@gmail.com>
2023-07-30 09:22:44 +0530

    A readme file was created and some print statements added to the source file

---
+++ README.md
@@ -0,0 +1,1 @@
+Hello this is a readme file for this project, it contains all the details of the project.
---
+++ first_file.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is my first file in the Git repository\n";
+
+    return 0;
+}
